c739249073dc5c1662f816d39c73610476a6037c
Максим <maksimaize@yandex.ru>
2026-04-17 17:04:31 +0300

    feat: не знаю что тут писать

---
+++ cource-1/practices/practice #2/practice #2/Publishing house (step 1).cs
@@ -0,0 +1,14 @@
+﻿public class PublishingHouse
+{
+    public string Name { get; set; }
+
+    public PublishingHouse(string name)
+    {
+        Name = name;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Издательство: {Name}");
+    }
+}
---
+++ cource-1/practices/practice #2/practice #2/Reservation (independent task).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public class Reservation
+{
+    public Book ReservedBook { get; }
+    public Reader ReservedBy { get; }
+    public DateTime ReservationDate { get; }
+    public int ExpiryDays { get; }
+
+    public Reservation(Book book, Reader reader, int expiryDays = 3)
+    {
+        ReservedBook = book;
+        ReservedBy = reader;
+        ReservationDate = DateTime.Now;
+        ExpiryDays = expiryDays;
+    }
+
+    public bool IsExpired()
+    {
+        return DateTime.Now > ReservationDate.AddDays(ExpiryDays);
+    }
+}
