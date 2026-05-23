0ed74595c31854a6c2f4737d97566e8e941428bd
Максим <maksimaize@yandex.ru>
2026-05-23 17:05:11 +0300

    feat: изменен урок 18(16) (методы заменены на асинхронные)

--- cource-1/practices/Lesson #18(16)/Lesson #18(16)/Program(step 2-5,step 8-11).cs
+++ cource-1/practices/Lesson #18(16)/Lesson #18(16)/Program(step 2-5,step 8-11).cs
@@ -2,41 +2,72 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-//steps 2-6
-var books = new List<Book>
+using System.Threading.Tasks;
+
+class Program
 {
-    new Book { Title = "War and Peace", Author = "Tolstoy", Year = 1869 },
-    new Book { Title = "Crime and Punishment", Author = "Dostoevsky", Year = 1866 }
-};
-
-var options = new JsonSerializerOptions { WriteIndented = true };
-File.WriteAllText("books.json", JsonSerializer.Serialize(books, options));
-
-var booksFromFile = JsonSerializer.Deserialize<List<Book>>(File.ReadAllText("books.json"));
-
-foreach (var book in booksFromFile)
-{
-    Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Year: {book.Year}");
-}
-
-// steps 8-11
-var library = new Library
-{
-    Name = "City Library",
-    Books = new List<Book>
+    static async Task Main(string[] args)
     {
-        new Book { Title = "War and Peace", Author = "Tolstoy", Year = 1869 },
-        new Book { Title = "Crime and Punishment", Author = "Dostoevsky", Year = 1866 }
+        // step 2
+        var books = new List<Book>
+        {
+            new Book { Title = "War and Peace", Author = "Lev Tolstoy", Year = 1869 },
+            new Book { Title = "Crime and Punishment", Author = "Fyodor Dostoevsky", Year = 1866 }
+        };
+
+        var options = new JsonSerializerOptions { WriteIndented = true };
+
+        // step 3
+        using (FileStream writeStreambook = File.Create("books.json"))
+        {
+            await JsonSerializer.SerializeAsync(writeStreambook, books, options);
+        }
+
+        // step 4
+        List<Book> booksFromFile;
+        using (FileStream readStreambook = File.OpenRead("books.json"))
+        {
+            booksFromFile = await JsonSerializer.DeserializeAsync<List<Book>>(readStreambook);
+        }
+
+        // step 5
+        Console.WriteLine("--- Books ---");
+        foreach (var book in booksFromFile)
+        {
+            Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Year: {book.Year}");
+        }
+
+        Console.WriteLine("\n--- Library ---");
+
+        // step 8
+        var library = new Library
+        {
+            Name = "City Library",
+            Books = new List<Book>
+            {
+                new Book { Title = "War and Peace", Author = "Lev Tolstoy", Year = 1869 },
+                new Book { Title = "Crime and Punishment", Author = "Fyodor Dostoevsky", Year = 1866 }
+            }
+        };
+
+        // step 9
+        using (FileStream writeStreamlib = File.Create("library.json"))
+        {
+            await JsonSerializer.SerializeAsync(writeStreamlib, library, options);
+        }
+
+        // step 10
+        Library libraryFromFile;
+        using (FileStream readStreamlib = File.OpenRead("library.json"))
+        {
+            libraryFromFile = await JsonSerializer.DeserializeAsync<Library>(readStreamlib);
+        }
+
+        // step 11
+        Console.WriteLine($"Library: {libraryFromFile.Name}");
+        foreach (var b in libraryFromFile.Books)
+        {
+            Console.WriteLine($"Book: \"{b.Title}\", Author: {b.Author}, Year: {b.Year}");
+        }
     }
-};
-
-File.WriteAllText("library.json", JsonSerializer.Serialize(library, options));
-
-var libraryFromFile = JsonSerializer.Deserialize<Library>(File.ReadAllText("library.json"));
-
-Console.WriteLine($"\nLibrary: {libraryFromFile.Name}");
-foreach (var b in libraryFromFile.Books)
-{
-    Console.WriteLine($"Book: \"{b.Title}\", Author: {b.Author}, Year: {b.Year}");
 }
