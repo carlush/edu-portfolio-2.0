a60c54e6575b5c75b4aaa847c423a773509214eb
Максим <maksimaize@yandex.ru>
2026-09-18 17:24:59 +0300

    feat:добавлено 2 проекта, оба в папке projects в cource-2

---
+++ cource-2/projects/TestingPlatform/TestingPlatform/Controllers/Questions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuestionsPlatform.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class QuestionsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAllQuestions() => Ok("Список вопросов");
+
+    [HttpGet("{id}")]
+    public IActionResult GetQuestionById(int id)
+    {
+        if (id == 1) return Ok("Вопрос 1");
+        return NotFound();
+    }
+
+    [HttpPost]
+    public IActionResult CreateQuestion() => Created("/api/Questions/1", "Создан вопрос с ID=1");
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateQuestions(int id) => NoContent();
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteQuestion(int id) => NoContent();
+}
