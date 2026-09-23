9cc85af7ed177dc0d950b14be09f36a2571f2e0a
Максим <maksimaize@yandex.ru>
2026-09-23 17:58:19 +0300

    мне лень писать, апдейт проекта короч

---
+++ cource-2/projects/TestingPlatform/TestingPlatform/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using practice.Models;
+
+namespace practice.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Student> Students => Set<Student>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // (необязательно) первичные данные, появятся после миграции
+        modelBuilder.Entity<Student>().HasData(
+            new Student { Id = 1, Login = "vanya123", Email = "vanya123@mail.com", FirstName = "Иван", MiddleName = "Иванович", LastName = "Иванов", Phone = "+71234567890", VkProfileLink = "http://vk.com/vanya123", CreatedAt = DateTimeOffset.Parse("2026-06-07T14:40:00+03:00") },
+            new Student { Id = 2, Login = "maria", Email = "maria@mail.com", FirstName = "Мария", MiddleName = "Ивановна", LastName = "Иванова", Phone = "+71234567899", VkProfileLink = "http://vk.com/vanya123", CreatedAt = DateTimeOffset.Parse("2026-06-09T16:30:00+03:00") }
+        );
+    }
+}
---
+++ cource-2/projects/TestingPlatform/TestingPlatform/Models/Student.cs
@@ -0,0 +1,52 @@
+﻿namespace practice.Models;
+
+/// <summary>
+/// Студент
+/// </summary>
+public class Student
+{
+    /// <summary>
+    /// Идентификатор студента
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Логин
+    /// </summary>
+    public string Login { get; set; }
+
+    /// <summary>
+    /// Email
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    /// Имя
+    /// </summary>
+    public string FirstName { get; set; }
+
+    /// <summary>
+    /// Отчество
+    /// </summary>
+    public string? MiddleName { get; set; }
+
+    /// <summary>
+    /// Фамилия
+    /// </summary>
+    public string LastName { get; set; }
+
+    /// <summary>
+    /// Номер телефона
+    /// </summary>
+    public string Phone { get; set; }
+
+    /// <summary>
+    /// Ссылка на профиль студента в ВК
+    /// </summary>
+    public string VkProfileLink { get; set; }
+
+    /// <summary>
+    /// Дата создания пользователя
+    /// </summary>
+    public DateTimeOffset CreatedAt { get; set; }
+}
